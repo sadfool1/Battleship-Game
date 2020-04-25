2bdd610c80b7ac97ae4b735442409a5278f59b8c
James <takw14@gmail.com>
2020-04-25 21:15:59 +0800

    Final Edit

--- Test/untitled25.py
+++
@@ -1,9 +0,0 @@
-omg = [1,3,4,1,4]
-
-hi = 1
-
-for iterations in range(len(omg)):
-    if hi == omg[iterations]:
-        print (True)
-    else:
-        print (False)
--- battleship_game.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from tkinter import * 
-from tkinter import messagebox
-from login_signup import *
-from player_placement import *
-
-main_account_screen()
